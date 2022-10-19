91908acf406aee1381089e2340ce7061077a0334
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-10-19 19:17:39 +0200

    Initial Commit!
    This is the initial commit for this experiment of a search engine. I
    hope I can learn a lot from this!

---
+++ README.md
@@ -0,0 +1,27 @@
+# OSSE (Overly Simple Searche Engine) (Oh-Si)
+
+An attempt to make a "functional" search engine while learning Rust :^)
+
+## Building
+
+### 1. Setup your environment
+
+#### With Nix:
+```
+$ nix develop
+```
+or
+```
+$ nix-shell shell.nix
+```
+
+#### Otherwise:
+
+Install cargo through your preferred method.
+
+
+### 2. Build!
+
+```
+$ cargo build
+```
---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "baitinq.github.io flake";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+
+}
