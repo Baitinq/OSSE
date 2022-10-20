e5627a4d6261adbd13be988d92705a1b24f40733
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-10-21 01:18:17 +0200

    Indexer: Add skeleton http rest endpoint functionality
    /search and /resource endpoint.

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
+use actix_web::{get, post, web, App, HttpServer, Responder};
+use serde::Deserialize;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     println!("Hello, world! Im the indexer!");
+
+    serve_http_endpoint("127.0.0.1", 4444).await
+}
+
+async fn serve_http_endpoint(address: &str, port: u16) -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(greet).service(add_resource))
+        .bind((address, port))?
+        .run()
+        .await
+}
+
+#[derive(Deserialize, Debug)]
+struct Resource {
+    url: String,
+    content: String,
+}
+
+#[post("/resource")]
+async fn add_resource(resource: web::Json<Resource>) -> impl Responder {
+    println!("Added resource! {:?}", resource);
+    format!("{:?}", resource)
+}
+
+#[get("/search/{term}")]
+async fn greet(term: web::Path<String>) -> impl Responder {
+    format!("Searching for: {term}")
 }
