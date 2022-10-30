57e36c148c00c339d7b1b3426b7ca651edfc8a1b
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-10-30 15:47:47 +0100

    Frontend: Setup skeleton route support
    We now have the / and /search/ route boilerplate. This will allow us to
    switch to the /search/ route when searching.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,6 +1,39 @@
 mod app;
 
-use app::App;
+use yew::prelude::*;
+use yew_router::prelude::*;
+use app::OSSE;
+
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    OSSEHome,
+    #[at("/search/:query")]
+    OSSESearch { query: String },
+}
+
+fn switch_routes(routes: &Route) -> Html {
+    match routes {
+        Route::OSSEHome => html! {
+            <OSSE api_endpoint={"http://127.0.0.1:4444"}/>
+        },
+        Route::OSSESearch { query } => html! {
+            <OSSE api_endpoint={"http://127.0.0.1:4444"}/>
+        },
+    }
+}
+
+#[function_component(App)]
+fn yew_app() -> Html {
+    html! {
+        <>
+            <BrowserRouter>
+                <Switch<Route> render={Switch::render(switch_routes)} />
+            </BrowserRouter>
+        </>
+    }
+}
 
 fn main() {
     yew::start_app::<App>();
